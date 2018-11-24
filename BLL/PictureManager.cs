525af87d26435124d26c59704b7197c8b853067b
Pierre Anken <pierre.anken@gmail.com>
2018-11-24 12:59:29 +0100

    Picture terminé

--- BLL/PictureManager.cs
+++ BLL/PictureManager.cs
@@ -1,13 +1,23 @@
-﻿using DAL;
+﻿using BLL.Helpers;
+using DAL;
 using DTO;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BLL
 {
     public class PictureManager
     {  
         public static List<Picture> GetPictureFromRoomId(int IdRoom) {
-            return PictureDB.GetPictureFromRoomId(IdRoom);
+            using (HttpClient httpClient = new HttpClient())
+            {
+                //GET: api/Picture/id
+                Task<string> response = httpClient.GetStringAsync(UrlHelper.ApiPictureUrl + IdRoom);
+                return JsonConvert.DeserializeObject<List<Picture>>(response.Result);
+            }
         }
+
     }
 }
---
+++ WebServiceRest/Controllers/PictureController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Web.Http;
+using System.Data.Entity;
+
+namespace WebServiceRest.Controllers
+{
+    public class PictureController : VBController
+    {
+        
+        //GET: api/Picture/id
+        public IHttpActionResult GetPictureFromRoomId(int id)
+        {
+            return Ok(DB.Pictures.Where(p => p.IdRoom == id).ToList());
+        }
+
+    }
+}
